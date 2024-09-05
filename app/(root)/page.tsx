98cadce83b1ee818222c06d3f43ceaaec90e2541
PRACHI1013 <prachiprajapati1002@gmail.com>
2024-09-05 18:26:49 +0530

    final push

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection'
-// import Search from '@/components/shared/Search';
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Search from '@/components/shared/Search';
 import Collection from '@/components/shared/Collection';
 import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
@@ -47,10 +46,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
-        {/* <div className="flex w-full flex-col gap-5 md:flex-row">
+         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
-        </div> */}
+        </div>
 
         <Collection 
           data={events?.data}
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,7 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
-//import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
 import { IOrder } from '@/lib/database/models/order.model'
 import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs/server'
@@ -16,16 +16,26 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  //const orders = await getOrdersByUser({ userId, page: ordersPage})
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
 
-  //const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
     <>
-      
+    {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-5">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">
+              Explore More Events
+            </Link>
+          </Button>
+        </div>
+      </section>
 
-      {/* <section className="wrapper my-8">
+     <section className="wrapper">
         <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
@@ -36,7 +46,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
         />
-      </section> */}
+      </section> 
 
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -63,17 +73,8 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         />
       </section>
 
-      {/* My Tickets */}
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">
-              Explore More Events
-            </Link>
-          </Button>
-        </div>
-      </section>
+      
+      
 
       
     </>
